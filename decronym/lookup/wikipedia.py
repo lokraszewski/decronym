ebd41e5254592773604bdd6225b7a7effb3ac3c0
Lukasz Okraszewski <neo1936@gmail.com>
2021-02-13 17:06:44 +0000

    upd: Change wiki parser to just pick first paragraph

--- decronym/lookup/wikipedia.py
+++ decronym/lookup/wikipedia.py
@@ -32,24 +32,22 @@ from typing import (
     TYPE_CHECKING,
 )
 
+
 class LookupWikipedia(Lookup):
     def validate(self):
         self.valid = is_url_valid(self.source)
-        
+
     def find_direct(self, key: str):
         key = key.casefold()
-        r = requests.get(f"{self.source}{key.upper()}" )
+        r = requests.get(f"{self.source}{key.upper()}")
         if r.status_code != 200:
             return []
 
-        
-        soup = BeautifulSoup(
-            r.text, "html.parser"
-        )
+        soup = BeautifulSoup(r.text, "html.parser")
 
         results = []
 
-        # This is a super naive approach for the proof of concept. 
+        # This is a super naive approach for the proof of concept.
         for t in soup.find_all("p"):
             if "may refer to" in t.text and key.upper() in t.text:
                 # Disambiguation
@@ -57,55 +55,69 @@ class LookupWikipedia(Lookup):
                     if "see also" in headline.text.casefold():
                         # Skip the disambiguation and similar matches
                         continue
-                    
-                    for item in headline.parent.findNext('ul').find_all("li"):
+
+                    for item in headline.parent.findNext("ul").find_all("li"):
                         a = item.find("a")
-                        if a and a.has_attr('title'):
-                            full = a['title']
+                        if a and a.has_attr("title"):
+                            full = a["title"]
                         else:
                             continue
                         comment = item.text
-                        results += [Result(
-                                acronym = key,
+                        results += [
+                            Result(
+                                acronym=key,
                                 full=full,
                                 source=self.source,
                                 comment=comment,
-                                tags=generate_tags(headline['id'].lower(), ["wiki"], mapping=self.config.get_tag_map()),
-                            )]
+                                tags=generate_tags(
+                                    headline["id"].lower(),
+                                    ["wiki"],
+                                    mapping=self.config.get_tag_map(),
+                                ),
+                            )
+                        ]
 
                 else:
                     # Has no headlines
                     for item in t.parent.findNext("ul").find_all("li"):
                         a = item.find("a")
-                        if a and a.has_attr('title'):
-                            full = a['title']
+                        if a and a.has_attr("title"):
+                            full = a["title"]
                         else:
                             continue
 
-                        tags=["wiki"]
+                        tags = ["wiki"]
                         comment = item.text
-                        results += [Result(
-                                acronym = key,
+                        results += [
+                            Result(
+                                acronym=key,
                                 full=full,
                                 source=self.source,
                                 comment=comment,
-                                tags=tags
-                            )]
+                                tags=generate_tags(
+                                    comment, ["wiki"], mapping=self.config.get_tag_map()
+                                ),
+                            )
+                        ]
                 break
         else:
-            for t in soup.find_all("p"):
-                for sentence in [sentence for sentence in t.text.split('.') if f"({key.upper()}" in sentence]:
-                    for full, _ in find_acronym_groups(sentence, key):
-                        results += [Result(
-                                    acronym = key,
-                                    full=full.strip(),
-                                    source=self.source,
-                                    comment=sentence,
-                                    tags=generate_tags(sentence, ["wiki"], mapping=self.config.get_tag_map())
-                                )]
-                        break
-                else:
-                    continue  # only executed if the inner loop did NOT break
-                break  # only executed if the inner loop DID break
-            
+            for sentence in [
+                sentence
+                for sentence in soup.find("p").text.split(".")
+                if f"({key.upper()}" in sentence
+            ]:
+                for full, _ in find_acronym_groups(sentence, key):
+                    results += [
+                        Result(
+                            acronym=key,
+                            full=full.strip(),
+                            source=self.source,
+                            comment=sentence,
+                            tags=generate_tags(
+                                sentence, ["wiki"], mapping=self.config.get_tag_map()
+                            ),
+                        )
+                    ]
+                    break
+
         return results
